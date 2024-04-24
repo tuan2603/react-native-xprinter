8ae02310114e03cae22c31c83ae0cf1a568089c5
tuan2603 <tuan260387@gmail.com>
2024-04-24 15:37:21 +0700

    Revert "Merge branch 'main' of github.com:tuan2603/react-native-xprinter"
    This reverts commit 7e5aae633d5d6b93f3f964ecfca017ac038ba2e7, reversing
    changes made to e2dfab0b6ce1764e0039099b1ae513e6578c553a.

--- ios/Xprinter-Bridging-Header.h
+++ ios/Xprinter-Bridging-Header.h
@@ -1,10 +1,2 @@
 #import <React/RCTBridgeModule.h>
 #import <React/RCTViewManager.h>
-#import "POSPrinterSDK.h"
-#import "POSWIFIManager.h"
-#import "PrinterProfile.h"
-#import "WIFIConnecter.h"
-
-
-
-
--- ios/Xprinter.swift
+++ ios/Xprinter.swift
@@ -1,47 +1,8 @@
 @objc(Xprinter)
-class Xprinter: NSObject, POSWIFIManagerDelegate {
-    
-    let manager = POSWIFIManager.sharedInstance()
-    
- var connectResolve: RCTPromiseResolveBlock?
- var connectReject: RCTPromiseRejectBlock?
-    
+class Xprinter: NSObject {
+
   @objc(multiply:withB:withResolver:withRejecter:)
   func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
     resolve(a*b)
   }
-    
-    
-    @objc(connect:withResolver:withRejecter:)
-    func connect(ipAddress: String, resolve:@escaping RCTPromiseResolveBlock,reject:@escaping RCTPromiseRejectBlock) -> Void {
-
-        connectResolve = resolve
-        connectReject = reject
-        
-        if ((manager?.isConnect) != nil) {
-            manager?.disconnect()
-        }
-
-        manager?.connect(withHost: ipAddress, port: 9100)
-    }
-    
-    func poSwifiConnected(toHost host: String!, port: UInt16) {
-        // Xử lý khi kết nối thành công
-        print("Connected to host: \(String(describing: host)) on port: \(port)")
-        connectResolve?(true)
-    }
-        
-    func poSwifiDisconnectWithError(_ error: Error!) {
-        // Xử lý khi kết nối bị ngắt
-        if let error = error {
-            print("Disconnected with error: \(error.localizedDescription)")
-            connectReject?("Error occurred: \(error.localizedDescription)", nil, error)
-        } else {
-            print("Disconnected without error")
-            connectResolve?(false)
-        }
-       
-    }
-
-
 }
